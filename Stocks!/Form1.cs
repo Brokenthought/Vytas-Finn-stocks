d0c6f5db812713b9e35c67996324d58cae26c5dc
Brokenthought <Brokenthought@users.noreply.github.com>
2015-01-13 07:36:10 +0000

    Update Form1.cs
    Lots of changes, will need to update the form and class files too

--- Stocks!/Form1.cs
+++ Stocks!/Form1.cs
@@ -14,142 +14,231 @@ namespace Stocks_
 {
     public partial class Form1 : Form
     {
-       
-        Random rnd = new Random();
-        PointPairList list1 = new PointPairList();
-        PointPairList list2 = new PointPairList();
-        PointPairList list3 = new PointPairList();
-        PointPairList list4 = new PointPairList();
-        PointPairList list5 = new PointPairList();
-        PointPairList list6 = new PointPairList();
-        PointPairList list7 = new PointPairList();
+             
+        List<PointPairList> pointList = new List<PointPairList>();
+        List<Stock> stocksList = new List<Stock>();
 
-
+        
         double x = 0;
+        int count = 0;
+        double highestStock = 0;
+        User user = new User("Lord Wimbley", 50000.00);
+
+
 
         public Form1()
         {
             InitializeComponent();
+
+            CreateStock();
+
             System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
-            t.Interval = 500;
+            t.Interval = 800;
             t.Tick += new EventHandler(timer_Tick);
             t.Start();
 
-            CreateGraph(zedGraph);
-            CreateGraph(zedGraphControl1);
-            CreateGraph(zedGraphControl2);
-            CreateGraph(zedGraphControl3);
-            CreateGraph(zedGraphControl4);
-            CreateGraph(zedGraphControl5);
-           
+
             zedGraph.GraphPane.YAxis.Scale.Max = 5;
-            zedGraphControl1.GraphPane.YAxis.Scale.Max = 5;
-            zedGraphControl2.GraphPane.YAxis.Scale.Max = 5;
-            zedGraphControl3.GraphPane.YAxis.Scale.Max = 5;
-            zedGraphControl4.GraphPane.YAxis.Scale.Max = 5;
-            zedGraphControl5.GraphPane.YAxis.Scale.Max = 5;
+            zedGraph.GraphPane.YAxis.Scale.Min = -1;
+            
+            foreach (Stock stock in stocksList)
+            {
+                stocksComboBx.Items.Add(stock.StockName);
+
+            }
+           
         }
+
 
 
 
         void timer_Tick(object sender, EventArgs e)
         {
-           
-            double rand = rnd.Next(3);
-            double rand1 = rnd.Next(4);
-            double rand2 = rnd.Next(2);
-            double rand3 = rnd.Next(2);
-            double rand4 = rnd.Next(5);
-            double rand5 = rnd.Next(3);
+            int stockNo;
+            double rand;
+            double totalValue = 0;
+            double stockValue = 0;
 
-            x = x + 0.2;
-            list1.Add(x, rand);
-            list3.Add(x, rand1);
-            list4.Add(x, rand2);
-            list5.Add(x, rand3);
-            list6.Add(x, rand4);
-            list7.Add(x, rand5);
-          
+
+            x += 0.2;
 
             zedGraph.GraphPane.XAxis.Scale.Max = x + 2;
-            zedGraphControl1.GraphPane.XAxis.Scale.Max = x + 2;
-            zedGraphControl2.GraphPane.XAxis.Scale.Max = x + 2;
-            zedGraphControl3.GraphPane.XAxis.Scale.Max = x + 2;
-            zedGraphControl4.GraphPane.XAxis.Scale.Max = x + 2;
-            zedGraphControl5.GraphPane.XAxis.Scale.Max = x + 2;
-
-            if (x > 10) 
+            if (x > 10)
             {
-                zedGraph.GraphPane.XAxis.Scale.Min = x - 10;
-                zedGraphControl1.GraphPane.XAxis.Scale.Min = x - 10;
-                zedGraphControl2.GraphPane.XAxis.Scale.Min = x - 10;
-                zedGraphControl3.GraphPane.XAxis.Scale.Min = x - 10;
-                zedGraphControl4.GraphPane.XAxis.Scale.Min = x - 10;
-                zedGraphControl5.GraphPane.XAxis.Scale.Min = x - 10;
+                zedGraph.GraphPane.XAxis.Scale.Min = x - 7;
+            }
+
+            int i = 0;
+
+            //Convert.ToInt16(stocksList.GetEnumerator().Current)
+
+           foreach (Stock stock in stocksList)
+            {
+                Random rnd = new Random();
+                rand = rnd.Next(-5,10);
+                              
+                pointList.ElementAt(i).Add(x, stock.StockValue + rand/10);
+                stock.StockValue += rand/10;
+                
+                i++;
+
+                if (stock.StockValue + rand / 10 > highestStock)
+               {
+                   highestStock = stock.StockValue + rand / 10;
+                   zedGraph.GraphPane.YAxis.Scale.Max = highestStock+2;
+               }
+   
+            }
+             zedGraph.Invalidate();
+             userNameBx.Text = user.UserName;
+             cashTxt.Text = user.Cash.ToString();
+
+
+             foreach (KeyValuePair<string, double> userStock in user.portfolio)
+             {
+
+                stockValue = stocksList.Find(name => name.StockName.Equals(userStock.Key)).StockValue;
+                totalValue += userStock.Value * stockValue;
+
+             }
+             stkValueTxt.Text = totalValue.ToString();
+
+
+             if (stocksComboBx.SelectedItem != null)
+             {
+                 stockValue = stocksList.Find(name => name.StockName.Equals(stocksComboBx.SelectedItem.ToString())).StockValue;
+                 stockNo = stocksList.Find(name => name.StockName.Equals(stocksComboBx.SelectedItem.ToString())).availableStocks;
+
+                 totalStkValueTxt.Text = (stockValue * stockNo).ToString();
+
+                 availSharesTxt.Text = stockNo.ToString();
+             }
+
+             
+
+        }
+
+
+        private void findStock(string stock)
+        {
+
+        }
+       
+        private void createBtn_Click_1(object sender, EventArgs e)
+        {
+            int num;
+   
+            if (stkNameTxt.Text != "" && int.TryParse(StkVauleTxt.Text, out num))
+            {
+                Stock stock = new Stock();
+
+                stock.StockName = stkNameTxt.Text;
+
+                stock.StockValue = num;
+
+                stocksList.Add(stock);
+
+                PointPairList pointPair = new PointPairList();
+                pointList.Add(pointPair);
+
+                zedGraph.GraphPane.AddCurve(stock.StockName,
+                pointList.ElementAt(count),
+                Color.FromName(colurComboBx.SelectedItem.ToString()), SymbolType.Diamond);
+                count++;
+
+                stocksComboBx.Items.Add(stock.StockName);
             }
             
-
-            zedGraph.Invalidate();
-            zedGraphControl1.Invalidate();
-            zedGraphControl2.Invalidate();
-            zedGraphControl3.Invalidate();
-            zedGraphControl4.Invalidate();
-            zedGraphControl5.Invalidate();
- 
-         
         }
 
-       
-        private void zedGraph_Load(object sender, EventArgs e)
+
+
+
+
+
+
+        private void CreateStock()
         {
-            
+            Stock stock = new Stock();
+
+            stock.StockName = "Monkey Paws";
+
+            stock.StockValue = 2;
+
+            stock.availableStocks = 100;
+
+            stocksList.Add(stock);
+
+            PointPairList pointPair = new PointPairList();
+            pointList.Add(pointPair);
+
+            LineItem myCurve = zedGraph.GraphPane.AddCurve(stock.StockName,
+            pointList.ElementAt(0),
+            Color.Blue, SymbolType.Diamond);
         }
-        private void CreateGraph(ZedGraphControl zgc)
+
+
+        void createGraph()
         {
+            ZedGraph.ZedGraphControl zeddo = new ZedGraph.ZedGraphControl();
+            GraphPane myPane = new GraphPane();
 
-            
-            LineItem myCurve = zedGraph.GraphPane.AddCurve("Google",
-            list1, Color.Red, SymbolType.Diamond);
+            myPane = zeddo.GraphPane;
 
-            LineItem myCurve1 = zedGraphControl1.GraphPane.AddCurve("Facebook",
-            list3, Color.Red, SymbolType.Diamond);
+            myPane.Title.Text = "This is an example!";
 
-            LineItem myCurve2 = zedGraphControl2.GraphPane.AddCurve("Intel",
-            list4, Color.Red, SymbolType.Diamond);
 
-            LineItem myCurve3 = zedGraphControl3.GraphPane.AddCurve("SAP",
-            list5, Color.Red, SymbolType.Diamond);
 
-            LineItem myCurve4 = zedGraphControl4.GraphPane.AddCurve("HP",
-            list6, Color.Red, SymbolType.Diamond);
-
-            LineItem myCurve5 = zedGraphControl5.GraphPane.AddCurve("Yahoo",
-            list7, Color.Red, SymbolType.Diamond);
-
-            
+            zeddo.AxisChange();
+            zeddo.Location = new Point(500, 0);
+            Controls.Add(zeddo);
         }
 
-        
-
-        private void zedGraph_Load_1(object sender, EventArgs e)
+        private void buyBtn_Click(object sender, EventArgs e)
         {
+            int num;
+            double stockValue;
+            int stockNo;
+            stockValue = stocksList.Find(name => name.StockName.Equals(stocksComboBx.SelectedItem.ToString())).StockValue;
+            stockNo = stocksList.Find(name => name.StockName.Equals(stocksComboBx.SelectedItem.ToString())).availableStocks;
 
+
+            if (int.TryParse(buyTxtBx.Text, out num) && user.Cash >= num * stockValue && num <= stockNo)
+            {
+                if (!user.portfolio.ContainsKey(stocksComboBx.SelectedItem.ToString()))
+                {
+                    user.portfolio.Add(stocksComboBx.SelectedItem.ToString(), num);
+                }
+                else
+                {
+                    user.portfolio[stocksComboBx.SelectedItem.ToString()] += num;
+                }
+
+                user.Cash -= num * stockValue;
+                stocksList.Find(name => name.StockName.Equals(stocksComboBx.SelectedItem.ToString())).availableStocks = stockNo - num;
+            }
         }
 
-        private void zedGraphControl1_Load(object sender, EventArgs e)
+        private void sellBtn_Click(object sender, EventArgs e)
         {
+            int num;
+            double stockValue;
 
+            if (int.TryParse(sellTxtBx.Text, out num) && user.portfolio[stocksComboBx.SelectedItem.ToString()] >= num)
+            {
+                stockValue = stocksList.Find(name => name.StockName.Equals(stocksComboBx.SelectedItem.ToString())).StockValue;
+
+                user.portfolio[stocksComboBx.SelectedItem.ToString()] -= num;
+
+                user.Cash += num * stockValue;
+
+                stocksList.Find(name => name.StockName.Equals(stocksComboBx.SelectedItem.ToString())).availableStocks += num;
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
 
 
- 
-      
-      
+    
+
     }
 }
-
